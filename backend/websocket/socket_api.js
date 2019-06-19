ccbc23c7abe60e24f97723824b243c2711797e37
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-06-19 13:12:53 +0900

    threadNameはブラウザから取得

--- backend/websocket/socket_api.js
+++ backend/websocket/socket_api.js
@@ -4,25 +4,22 @@ const io = socketIO();
 
 const comments = io.of('/comments');
 comments.on('connection', (socket) => {
-  var threadName = '';
-
   socket.on('thread_join', (data) => {
     console.log(data);
 
-    threadName = data.value;
-    socket.join(threadName);
+    socket.join(data.threadName);
   });
 
   socket.on('to_thread', (data) => {
     console.log(data);
 
-    comments.to(threadName).emit('to_thread_client', { value: data.value });
+    comments.to(data.threadName).emit('to_thread_client', { value: data.value });
   });
 
   socket.on('to_thread_broadcast', (data) => {
     console.log(data);
 
-    socket.broadcast.to(threadName).emit('to_thread_client', { value: data.value });
+    socket.broadcast.to(data.threadName).emit('to_thread_client', { value: data.value });
   });
 });
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,9 +4,9 @@ import io from 'socket.io-client';
 const Index = () => {
   useEffect(() => {
     const comments = io('http://localhost:3000/comments');
-    comments.emit('thread_join', { value: 'fire' });
-    comments.emit('to_thread', { value: 'Comment' });
-    comments.emit('to_thread_broadcast', { value: 'Musashi Joined!' });
+    comments.emit('thread_join', { threadName: 'fire' });
+    comments.emit('to_thread', { value: 'Comment', threadName: 'fire' });
+    comments.emit('to_thread_broadcast', { value: 'Musashi Joined!', threadName: 'fire' });
     comments.on('to_thread_client', (data) => {
       console.log(data.value);
     });
