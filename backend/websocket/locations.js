00deff5616e507556f76281f94a96938461bf0b4
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-06-20 16:20:52 +0900

    locationはroom無しに変更

--- backend/websocket/locations.js
+++ backend/websocket/locations.js
@@ -3,10 +3,6 @@ const Location = require('../models').location;
 module.exports = (io) => {
   const locations = io.of('/locations');
   locations.on('connection', (socket) => {
-    socket.on('thread_join', (data) => {
-      socket.join(data.thread_id);
-    });
-
     socket.on('upsert_location', async (data) => {
       const {
         user_id, thread_id, longitude, latitude
@@ -22,10 +18,10 @@ module.exports = (io) => {
       }
       catch (error) {
         console.log(error);
-        locations.to(data.thread_id).emit('upsert_location_client', { error });
+        locations.emit('upsert_location_client', { error });
         return;
       }
-      locations.to(data.thread_id).emit('upsert_location_client', {
+      locations.emit('upsert_location_client', {
         location: {
           user_id,
           thread_id,
