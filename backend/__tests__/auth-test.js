f31b21f901d2149af0aaded9f08f6217f6d26e94
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-06-24 13:19:11 +0900

    logout のapi テスト

--- backend/__tests__/auth-test.js
+++ backend/__tests__/auth-test.js
@@ -20,6 +20,54 @@ const loginUser = async (username, password) => {
   }
 };
 
+describe('GET: /api/v1/logout', () => {
+  let retrievedToken;
+  beforeAll(async () => {
+    jest.setTimeout(10000);
+    try {
+      const res = await loginUser('username1', 'password');
+      retrievedToken = res.body.token;
+    }
+    catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+      throw error;
+    }
+  });
+
+  test('success', async () => {
+    jest.setTimeout(10000);
+    try {
+      const res = await request
+        .get('/api/v1/logout')
+        .set('Authorization', `Bearer ${retrievedToken}`);
+      expect(res.status).toBe(200);
+    }
+    catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+      throw error;
+    }
+  });
+
+  test('failure: request without token', async () => {
+    jest.setTimeout(10000);
+    try {
+      const res = await request
+        .get('/api/v1/logout');
+      expect(res.status).toBe(401);
+    }
+    catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+      throw error;
+    }
+  });
+});
+
 describe('POST: /api/v1/login', () => {
   test('success', async () => {
     jest.setTimeout(10000);
