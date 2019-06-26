5718681c6e43743a6e0770aba27258f64ca47eab
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-06-26 11:25:16 +0900

    form-testをshallowに変更

--- frontend/__tests__/form-test.js
+++ frontend/__tests__/form-test.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { createShallow, createMount } from '@material-ui/core/test-utils';
+import { createShallow } from '@material-ui/core/test-utils';
 import { TextField, Button } from '@material-ui/core';
 
 import Form from '../components/Form';
 
 describe('ログイン・サインアップフォーム', () => {
   let shallow;
-  let mount;
   beforeEach(() => {
     shallow = createShallow();
-    mount = createMount();
   });
 
   it('ログイン画面表示', () => {
-    const wrapper = mount(<Form isLogin />);
+    const wrapper = shallow(<Form isLogin />).dive();
 
     expect(wrapper.find(TextField).length).toBe(2);
     expect(wrapper.find(Button).text()).toBe('Login');
   });
 
   it('サインアップ画面表示', () => {
-    const wrapper = mount(<Form isLogin={false} />);
+    const wrapper = shallow(<Form isLogin={false} />).dive();
 
     expect(wrapper.find(TextField).length).toBe(3);
     expect(wrapper.find(Button).text()).toBe('Signup');
@@ -28,13 +26,13 @@ describe('ログイン・サインアップフォーム', () => {
 
   it('ログインにおいてテキストを入力するとボタンをクリックした時にコールバック呼び出しされる', () => {
     const onSubmit = jest.fn();
-    const wrapper = mount(<Form isLogin onSubmit={onSubmit} />);
-    wrapper.find(TextField).first().find('input').simulate('change', {
+    const wrapper = shallow(<Form isLogin onSubmit={onSubmit} />).dive();
+    wrapper.find(TextField).first().simulate('change', {
       target: {
         value: 'username'
       }
     });
-    wrapper.find(TextField).at(1).find('input').simulate('change', {
+    wrapper.find(TextField).at(1).simulate('change', {
       target: {
         value: 'password'
       }
@@ -46,13 +44,13 @@ describe('ログイン・サインアップフォーム', () => {
   it('ログインにおいてテキストを入力しないと関数が呼び出されない', () => {
     const onSubmit = jest.fn();
     const enqueueSnackbar = jest.fn();
-    const wrapper = mount(<Form isLogin onSubmit={onSubmit} enqueueSnackbar={enqueueSnackbar} />);
-    wrapper.find(TextField).first().find('input').simulate('change', {
+    const wrapper = shallow(<Form isLogin onSubmit={onSubmit} enqueueSnackbar={enqueueSnackbar} />).dive();
+    wrapper.find(TextField).first().simulate('change', {
       target: {
         value: ''
       }
     });
-    wrapper.find(TextField).at(1).find('input').simulate('change', {
+    wrapper.find(TextField).at(1).simulate('change', {
       target: {
         value: 'password'
       }
@@ -64,18 +62,18 @@ describe('ログイン・サインアップフォーム', () => {
 
   it('サインアップにおいてテキストを入力するとボタンをクリックした時にコールバック呼び出しされる', () => {
     const onSubmit = jest.fn();
-    const wrapper = mount(<Form isLogin={false} onSubmit={onSubmit} />);
-    wrapper.find(TextField).first().find('input').simulate('change', {
+    const wrapper = shallow(<Form isLogin={false} onSubmit={onSubmit} />).dive();
+    wrapper.find(TextField).first().simulate('change', {
       target: {
         value: 'email@email.com'
       }
     });
-    wrapper.find(TextField).at(1).find('input').simulate('change', {
+    wrapper.find(TextField).at(1).simulate('change', {
       target: {
         value: 'username'
       }
     });
-    wrapper.find(TextField).at(2).find('input').simulate('change', {
+    wrapper.find(TextField).at(2).simulate('change', {
       target: {
         value: 'password'
       }
@@ -87,18 +85,18 @@ describe('ログイン・サインアップフォーム', () => {
   it('サインアップにおいてテキストを入力しないと関数が呼び出されない', () => {
     const onSubmit = jest.fn();
     const enqueueSnackbar = jest.fn();
-    const wrapper = mount(<Form isLogin={false} onSubmit={onSubmit} enqueueSnackbar={enqueueSnackbar} />);
-    wrapper.find(TextField).first().find('input').simulate('change', {
+    const wrapper = shallow(<Form isLogin={false} onSubmit={onSubmit} enqueueSnackbar={enqueueSnackbar} />).dive();
+    wrapper.find(TextField).first().simulate('change', {
       target: {
         value: ''
       }
     });
-    wrapper.find(TextField).at(1).find('input').simulate('change', {
+    wrapper.find(TextField).at(1).simulate('change', {
       target: {
         value: ''
       }
     });
-    wrapper.find(TextField).at(2).find('input').simulate('change', {
+    wrapper.find(TextField).at(2).simulate('change', {
       target: {
         value: 'password'
       }
